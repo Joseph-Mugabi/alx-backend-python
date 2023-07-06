42a66ed968ba48ecbb70c641518a68385dcf11cc
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-07-06 15:21:07 +0300

    type-annotated function floor, takes a float n as arg and returns floor of float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+type-annotated function floor which takes a float n as argument
+returns the floor of the float.
+"""
+
+
+def to_str(n: float) -> float:
+    """ returns the floor of the float."""
+    return str(n)
