5d584173a800e279b012dbb5d2f504710ee6f8d3
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-07-06 16:47:42 +0300

    fixed from [float, int] to [int, float]

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,6 +6,6 @@ floats and returns their sum as a float.
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """returns their sum as a float."""
     return float(sum(mxd_lst))
