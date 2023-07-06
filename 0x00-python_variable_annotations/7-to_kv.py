3424475cf335f52c7780603ce797aa0946952d58
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-07-06 16:23:00 +0300

    func takes int or float as args and rtrns a tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+returns a tuple
+"""
+
+from typing import List, Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[int, float]:
+    """returns a tuple"""
+    return (k, float(v ** 2))
