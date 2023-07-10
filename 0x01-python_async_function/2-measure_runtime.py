356308f71ffd2ee3685a9458b47c389104ef8df2
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-07-10 14:27:44 +0300

    measure time func with 2 int args numb of tasks and max delay, returns a float

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+returns average time of execution of all asynchronous operations
+"""
+from time import perf_counter
+import asyncio
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """ returns average time """
+    begin = perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    stop = perf_counter()
+    return (stop - begin)
