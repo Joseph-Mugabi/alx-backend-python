810a6b29187806fa89cc80a3fcdc9734f69de10d
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-07-27 16:17:25 +0300

    test get json Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -35,3 +35,23 @@ class TestAccessNestedMap(TestCase):
         """tests raise of key error exception"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(TestCase):
+    """implementing testgetjson class that inherits from utils"""
+
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+    )
+    def test_get_json(self, test_url: str, expected: Dict):
+        """method test get json"""
+        mock_get = Mock()
+        mock_get.json.return_value = expected
+
+        with patch("requests.get", return_value=mock_get) as mock_request:
+            output_result = get_json(test_url)
+            self.assertEqual(output_result, expected)
+            mock_request.assert_called_once()
