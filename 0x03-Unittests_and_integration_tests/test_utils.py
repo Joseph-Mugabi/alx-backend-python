e2e5830972d1c0babada233e89b62d288f3b3ed3
Joseph-Mugabi <mugabijoshgreen@gmail.com>
2023-07-27 15:46:19 +0300

    test access nested map exception error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,10 +16,22 @@ class TestAccessNestedMap(TestCase):
         [
             ({"a": 1}, ("a",), 1),
             ({"a": {"b": 2}}, ("a",), {"b": 2}),
-            ({"a": {"b": 2}}, ("a", "b"), 2)
+            ({"a": {"b": 2}}, ("a", "b"), 2),
         ]
     )
     def test_access_nested_map(self, nested_map: Dict,
                                path: Tuple, expected: Union[int, str]):
         """test access_nested map func"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",)),
+            ({"a": 1}, ("a", "b")),
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map: Dict,
+                                         path: Tuple):
+        """tests raise of key error exception"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
